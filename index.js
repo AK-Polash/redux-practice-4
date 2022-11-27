c5ecd90f59dd56b068d44d54ed632796f8fae247
AK-Polash <polashk199@gmail.com>
2022-11-28 03:32:35 +0600

    redux one

---
+++ index.js
@@ -0,0 +1,42 @@
+let { createStore } = require("redux");
+let ADD_USER = "ADD_USER";
+
+// State:
+let initialState = {
+  count: 1,
+  users: ["Polash"],
+};
+
+// Action:
+let addUserAction = (value) => {
+  return {
+    type: ADD_USER,
+    payload: value,
+  };
+};
+
+// Reducer:
+let addUserReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_USER:
+      return {
+        count: state.count + 1,
+        users: [...state.users, action.payload],
+      };
+
+    default:
+      state;
+  }
+};
+
+// Store:
+let store = createStore(addUserReducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+// Dispatch Actions:
+store.dispatch(addUserAction("Dulon"));
+store.dispatch(addUserAction("Rakib"));
+store.dispatch(addUserAction("Emon"));
